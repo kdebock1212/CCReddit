f1107827304e5eeb086e952ae699884c13b7dbea
Kato De Bock <k.debock@alternate.be>
2024-01-25 16:46:31 +0100

    error handling related to API rate limits

--- src/Components/MainBody.js
+++ src/Components/MainBody.js
@@ -9,21 +9,53 @@ import SubRedditFeature from './Features/SubReddit';
 
 const MainBody = ({ selectedCategory }) => {
   const [redditData, setRedditData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [cachedResults, setCachedResults] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+
       try {
-        const response = await fetch(`https://www.reddit.com/r/${selectedCategory}.json?raw_json=1`);
-        const data = await response.json();
-        setRedditData(data.data.children);
+        if (cachedResults && selectedCategory in cachedResults) {
+          setRedditData(cachedResults[selectedCategory]);
+        } else {
+          const response = await fetch(`https://www.reddit.com/r/${selectedCategory}.json?raw_json=1`);
+          
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+
+          const data = await response.json();
+          setRedditData(data.data.children);
+
+          // Cache the successful query results
+          setCachedResults((prevResults) => ({
+            ...prevResults,
+            [selectedCategory]: data.data.children,
+          }));
+        }
       } catch (error) {
         console.error('Error fetching Reddit data:', error);
+        setError(error.message || 'An error occurred while fetching Reddit data.');
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchData();
-  }, [selectedCategory]);
-  
+  }, [selectedCategory, cachedResults]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div className='RedditFeed'>
       {/* Render the Reddit data using feature components */}
